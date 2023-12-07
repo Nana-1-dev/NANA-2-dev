433e919b2efa7e03b318669a421a297942804805
Nana-1-dev <132481925+Nana-1-dev@users.noreply.github.com>
2023-12-07 11:15:08 +0100

    premier commit

---
+++ src/main/java/com/supermarches/AsupermarcheBackendApplication.java
@@ -0,0 +1,13 @@
+package com.supermarches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsupermarcheBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsupermarcheBackendApplication.class, args);
+	}
+
+}
